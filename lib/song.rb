6d25c1f54ee315dd5d45d82de0ac0ba16c1e1459
Douglas Uretsky <douglasuretsky@gmail.com>
2018-10-09 15:23:17 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -49,7 +49,7 @@ class Song
   def self.new_from_filename(filename)
     parsed = filename.split(" - ")
     name = parsed[0]
-    p parsed[1].split(".")
+    title = parsed[1].split(".")
   end
 
 end
